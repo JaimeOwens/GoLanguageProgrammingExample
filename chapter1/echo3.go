77cb29001066f27e02ecca1ce02773de11380435
JaimeOwens <1139937387@qq.com>
2021-01-11 16:37:56 +0800

    chapter 1

---
+++ chapter1/echo3.go
@@ -0,0 +1,11 @@
+package chapter1
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Echo3() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
---
+++ chapter1/helloworld.go
@@ -0,0 +1,7 @@
+package chapter1
+
+import "fmt"
+
+func Helloworld() {
+	fmt.Println("Hello world!")
+}
